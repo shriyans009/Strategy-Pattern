2be7de04dbcfce2221299b8083c6340ba5b6fda0
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-09 15:17:04 +0530

    Updated swimBehaviour in Duck.java

--- Duck.java
+++ Duck.java
@@ -5,9 +5,17 @@ abstract public class Duck {
         this.quackBehaviour = quackBehaviour;
     }
 
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
+        this.swimBehaviour = swimBehaviour;
+    }
+
     abstract void display();
 
     public void performQuack(){
         quackBehaviour.quack();
+    }
+
+    public void performSwim(){
+        swimBehaviour.swim();
     }
 }
