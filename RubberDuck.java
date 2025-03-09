b0e4fae98ecb4efd546682d7b08a59d0ba275319
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-09 15:28:18 +0530

    Updated flyBehaviour in RubberDuck.java

--- RubberDuck.java
+++ RubberDuck.java
@@ -3,6 +3,7 @@ public class RubberDuck extends Duck{
     public RubberDuck(){
         quackBehaviour = new Squeak();
         swimBehaviour = new Floating();
+        flyBehaviour = new NotFly();
     }
 
     @Override
