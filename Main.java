c448a714743d021599f7327010054475ceeb9a59
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-09 15:04:35 +0530

    Created Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        RubberDuck rb  = new RubberDuck();
+        System.out.println("---------------");
+        rb.display();
+        rb.performQuack();
+        System.out.println("---------------");
+
+    }
+}
